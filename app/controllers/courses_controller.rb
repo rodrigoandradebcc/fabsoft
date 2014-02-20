ff3a71b2c1ab4a6f67445ea3f964bdf935b9cfee
daniel-leal <danielleal94@gmail.com>
2014-02-20 09:46:20 -0300

    alunos cadastrados - find by id

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,5 +1,5 @@
 class CoursesController < ApplicationController
-  before_action :set_course, only: [:show, :edit, :update, :destroy ]
+  before_action :set_course, only: [:show, :edit, :update, :destroy, :alunos_cadastrados]
 
   # GET /courses
   # GET /courses.json
